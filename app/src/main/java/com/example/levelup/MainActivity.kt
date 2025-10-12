a4b9d76884da9b2d372d24529a80622ba9264964
Rafael <raf.rios@duocuc.cl>
2025-10-12 13:06:18 -0300

    Avance en Pantalla de Contacto

--- app/src/main/java/com/example/levelup/MainActivity.kt
+++ app/src/main/java/com/example/levelup/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.levelup.ui.PantallaContacto
 import com.example.levelup.ui.theme.LevelUpTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,9 +20,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LevelUpTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
-                }
+                PantallaContacto()
             }
         }
     }
--- app/src/main/java/com/example/levelup/ui/PantallaContacto.kt
+++ app/src/main/java/com/example/levelup/ui/PantallaContacto.kt
@@ -1,4 +1,26 @@
 package com.example.levelup.ui
 
-class PantallaContacto {
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.material3.*
+import com.example.levelup.R
+
+@Composable
+fun PantallaContacto() {
+    Scaffold (
+        containerColor = Color.Black
+    ){innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .background(Color.Black)
+        ) {  }
+    }
+
 }
