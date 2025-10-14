76b1d13c378dc0d126d7cc706cf177312ab4f3e4
squezada <savkita.quezada@gmail.com>
2025-10-14 11:32:59 -0300

    Avance Productos y funciones

--- app/src/main/java/com/example/levelup/model/remote/ProductosApiService.kt
+++ app/src/main/java/com/example/levelup/model/remote/ProductosApiService.kt
@@ -1,4 +1,10 @@
 package com.example.levelup.model.remote
 
+
+// Esta interfaz simula un punto para futuras llamadas a APIs REST.
+// Puedes implementar Retrofit aquí si luego sincronizas los gastos con un servidor.
 interface ProductosApiService {
+
+    // suspend fun obtenerProductosRemotos(): List<ProductosDto>
+    // suspend fun enviarProductos(productos: ProductosDto)
 }
--- app/src/main/java/com/example/levelup/model/repository/ProductosRepository.kt
+++ app/src/main/java/com/example/levelup/model/repository/ProductosRepository.kt
@@ -1,4 +1,44 @@
 package com.example.levelup.model.repository
 
-class ProductosRepository {
+import com.example.levelup.model.local.ProductosDao
+import com.example.levelup.model.local.ProductosEntity
+import kotlinx.coroutines.flow.Flow
+
+
+class ProductosRepository (private val dao: ProductosDao){
+
+
+    fun observarProductos(): Flow<List<ProductosEntity>> = dao.observarTodos()
+
+    suspend fun obtener(id: Int) = dao.obtenerPorId(id)
+
+    suspend fun guardar(
+        id: Int?,
+        descripcion: String,
+        monto: Double,
+        categoria: String
+    ) {
+        if (id == null || id == 0) {
+            dao.insertar(
+                ProductosEntity(
+                    descripcion = descripcion.trim(),
+                    monto = monto,
+                    categoria = categoria.trim()
+                )
+            )
+        } else {
+            dao.actualizar(
+                ProductosEntity(
+                    id = id,
+                    descripcion = descripcion.trim(),
+                    monto = monto,
+                    categoria = categoria.trim()
+                )
+            )
+        }
+    }
+
+    suspend fun eliminar(productos: ProductosEntity) = dao.eliminar(productos)
+    suspend fun eliminarTodos() = dao.eliminarTodos()
+
 }
