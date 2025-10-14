b2e1117f566e501ec034a8351fc7608a15ebc8e6
Rafael <raf.rios@duocuc.cl>
2025-10-13 23:39:37 -0300

    Incorporando barra de navegación y resolviendo bugs con iconos y footer

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
 
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2024.10.01"))
-
+    implementation("androidx.compose.material:material-icons-extended:1.7.8")
     implementation("androidx.compose.ui:ui")
 
     implementation("androidx.compose.material3:material3")
@@ -57,6 +57,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
