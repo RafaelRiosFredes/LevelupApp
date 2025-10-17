be2d281c3c29bdd003de263405cfd8e2bf77db14
FranciscaA <fr.arancibiac@duocuc.cl>
2025-10-17 19:17:21 -0300

    Modificaciones en LoginScreen

--- app/src/main/java/com/example/levelup/ui/LoginScreen.kt
+++ app/src/main/java/com/example/levelup/ui/LoginScreen.kt
@@ -1,6 +1,8 @@
 package com.example.levelup.ui
 
-import android.R
+import android.R.attr.shape
+import com.example.levelup.R
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
@@ -8,6 +10,10 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -17,6 +23,8 @@ import com.example.levelup.viewmodel.LoginViewModel
 @Composable
 fun LoginScreen(vm: LoginViewModel, function: () -> Unit) {
     val form by vm.form.collectAsState()
+    val Russo_One = FontFamily(Font(R.font.russo_one))
+
 
     Surface(
         modifier = Modifier.fillMaxSize(),
@@ -25,14 +33,29 @@ fun LoginScreen(vm: LoginViewModel, function: () -> Unit) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(24.dp),
+                .padding(40.dp),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+
+            Image(
+                painter = painterResource(id = R.drawable.levelup2),
+                contentDescription = "Logo LevelUp",
+                modifier = Modifier
+                    .size(250.dp)
+                    .padding(top = 5.dp),
+                    contentScale = ContentScale.Fit
+
+            )
+
+
+            //Titulo
             Text(
                 text = "Hola! Inicia sesion",
                 color = Color.White,
-                style = MaterialTheme.typography.headlineMedium
+                fontSize = 22.dp.value.sp,
+                fontFamily = Russo_One,
+                style = MaterialTheme.typography.titleLarge
             )
 
             Spacer(Modifier.height(24.dp))
@@ -41,33 +64,44 @@ fun LoginScreen(vm: LoginViewModel, function: () -> Unit) {
                 value = form.correo,
                 onValueChange = vm::onChangeCorreo,
                 label = { Text("Ingresa tu correo", color = Color.White) },
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .width(350.dp)
+                    .height(80.dp)
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp),
+                shape = RoundedCornerShape(20.dp),
                 colors = TextFieldDefaults.colors(
                     focusedContainerColor = Color.Black,
                     unfocusedContainerColor = Color.Black,
-                    focusedIndicatorColor = Color.Green,
+                    focusedIndicatorColor = Color.Blue,
                     unfocusedIndicatorColor = Color.Gray,
-                    cursorColor = Color.Green,
-                    focusedLabelColor = Color.Green,
+                    cursorColor = Color.Blue,
+                    focusedLabelColor = Color.Blue,
                     unfocusedLabelColor = Color.White,
                     focusedTextColor = Color.White,
                     unfocusedTextColor = Color.White
                 )
             )
+            Spacer(Modifier.height(24.dp))
 
             OutlinedTextField(
                 value = form.contrasena,
                 onValueChange = vm::onChangeContrasena,
                 label = { Text("Ingresa tu Contraseña", color = Color.White) },
                 visualTransformation = PasswordVisualTransformation(),
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .width(350.dp)
+                    .height(80.dp)
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp),
+                shape = RoundedCornerShape(20.dp),
                 colors = TextFieldDefaults.colors(
                     focusedContainerColor = Color.Black,
                     unfocusedContainerColor = Color.Black,
-                    focusedIndicatorColor = Color.Green,
+                    focusedIndicatorColor = Color.Blue,
                     unfocusedIndicatorColor = Color.Gray,
-                    cursorColor = Color.Green,
-                    focusedLabelColor = Color.Green,
+                    cursorColor = Color.Blue,
+                    focusedLabelColor = Color.Blue,
                     unfocusedLabelColor = Color.White,
                     focusedTextColor = Color.White,
                     unfocusedTextColor = Color.White
@@ -79,7 +113,7 @@ fun LoginScreen(vm: LoginViewModel, function: () -> Unit) {
                 Text(it, color = if (form.exito) Color.Green else Color.Red)
             }
 
-            Spacer(Modifier.height(24.dp))
+            Spacer(Modifier.height(30.dp))
 
             Button(
                 onClick = { vm.validarUsuario() },
@@ -87,14 +121,15 @@ fun LoginScreen(vm: LoginViewModel, function: () -> Unit) {
                     containerColor = Color.Green,
                     contentColor = Color.Black
                 ),
-                shape = RoundedCornerShape(12.dp),
+                shape = RoundedCornerShape(20.dp),
                 modifier = Modifier
-                    .width(180.dp)
-                    .height(45.dp)
+                    .width(150.dp)
+                    .height(40.dp)
             ) {
                 Text(
                     text = "Ingresar",
                     fontSize = 16.sp,
+                    fontFamily = Russo_One,
                     style = MaterialTheme.typography.labelLarge
 
                 )
