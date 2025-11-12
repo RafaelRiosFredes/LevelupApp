7ac69123deb627370adc5e2e787d693947fa65d6
squezada <savkita.quezada@gmail.com>
2025-11-12 09:59:34 -0300

    Agregando pantalla noticias

--- app/src/main/java/com/example/levelup/model/data/NoticiaEntity.kt
+++ app/src/main/java/com/example/levelup/model/data/NoticiaEntity.kt
@@ -1,9 +1,10 @@
 package com.example.levelup.model.data
 
-data class NoticiaEntity {
+data class NoticiaEntity (
     val id: Int,
     val titulo: String,
     val descripcion: String,
     val imagen: String,
     val fuente: String
     )
+
